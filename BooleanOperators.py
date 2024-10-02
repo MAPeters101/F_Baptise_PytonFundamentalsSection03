18b78dc805034f69620f36911c3515d350e3227d
Mark Peters <mapeters101@earthlink.net>
2024-10-02 15:31:50 -0400

    Lesson034:  Boolean Operators:  Work with boolean operators.

---
+++ BooleanOperators.py
@@ -0,0 +1,27 @@
+print(not True)
+print(not False)
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+balance = 1000.00
+withdrawal = 50.00
+withdrawal_limit = 500.00
+print((withdrawal < withdrawal_limit) and (withdrawal <= balance))
+
+a = 20
+b = 30
+print(a/b>1)
+
+a = 20
+b = 0
+#print(a/b>1)
+print(b!=0 and a/b>1)
+
+
+
