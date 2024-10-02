7c4a4f95d9a7acc7b3f4d1cf70de7cf1390ca397
Mark Peters <mapeters101@earthlink.net>
2024-10-02 01:10:37 -0400

    Lesson024:  Variables:  Work with variables.

---
+++ variables.py
@@ -0,0 +1,35 @@
+a = 100
+
+print(a)
+print(a+11)
+b = a+11
+print(b)
+
+a = 3.14
+print(a)
+
+test = 100
+test_1 = 10
+_test_1_ = 10
+__test__ = 10
+TEST = 10
+
+print(test, TEST)
+
+
+a = float(10)
+print(a)
+float = 100.5
+print(float)
+# a = float(10)
+# print(a)
+
+del float
+a = float(10)
+print(a)
+
+current_balance = 100.0
+currentBalance = 100.0
+cb = 100.0
+
+
