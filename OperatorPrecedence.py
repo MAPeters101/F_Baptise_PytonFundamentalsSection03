d0759de732df3fd06b595dac4a0ed60d87d411cb
Mark Peters <mapeters101@earthlink.net>
2024-10-02 10:31:20 -0400

    Lesson028:  Operator Precedence:  Work with Operator Precedence.

---
+++ OperatorPrecedence.py
@@ -0,0 +1,31 @@
+print(100 - 20 + 50)
+print((100 - 20) + 50)
+print((100 + 50) - 20)
+
+print(10 * 4 + 5 * 5)
+print((10 * 4) + (5 * 5))
+
+print(10 * 2 ** 3)
+print(10 * (2 ** 3))
+
+principal = 100
+apr = 0.1
+years = 10
+future_value = principal * ((1 + apr/12) ** (years * 12))
+print(future_value)
+future_value = principal * (1 + apr/12) ** (years * 12)
+print(future_value)
+
+print(2 ** -8)
+print(2 ** (-8))
+
+print(-4 ** 0.5)
+print(-(4 ** 0.5))
+print((-4) ** 0.5)
+
+n = 10
+print(n * n - 1)
+print((n * n) - 1)
+print(n * (n - 1))
+
+
